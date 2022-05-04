81b4adc890b8b503d47d2d54c689e65f662aab73
unknown <lithos.contact@gmail.com>
2022-05-04 17:17:20 +0200

    Added the start screen

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,35 +1,105 @@
 <template>
-    <div class="start__container">
-        <h1 id="start__message"></h1>
-    </div>
+  <div class="start__container fixed__centered">
+    <h1 id="start__message--primary"></h1>
+    <h5 id="start__message--secondary"></h5>
+    <h2 class="fadeIn" v-if="x === STAGES.length">(Press any key to continue)</h2>
+  </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from "vue";
+import router from "../router";
 
 let i = ref(0);
+let x = ref(0);
+
+// Stages array
+const STAGES = reactive([
+  "MESSAGE_STAGE0A",
+  "MESSAGE_STAGE0B",
+  "MESSAGE_STAGE0C",
+  "MESSAGE_STAGE1",
+  "MESSAGE_STAGE2",
+  "MESSAGE_STAGE3",
+  "MESSAGE_STAGE4",
+  "MESSAGE_STAGE5",
+  "MESSAGE_STAGE6",
+  "MESSAGE_STAGE7",
+  "MESSAGE_STAGE8",
+  "MESSAGE_STAGE9",
+]);
+
+const getStageMessage = (stage) => {
+  const stages = {
+    ["MESSAGE_STAGE0A"]: "█",
+    ["MESSAGE_STAGE0B"]: "█",
+    ["MESSAGE_STAGE0C"]: "█",
+    ["MESSAGE_STAGE1"]:
+      "█ █ █ █ █ █ S T A R T I N G | S Y S T E M S █ █ █ █ █ █",
+    ["MESSAGE_STAGE2"]: "Loading libraries of human understanding...",
+    ["MESSAGE_STAGE3"]: "Subject No. 7,567,234",
+    ["MESSAGE_STAGE4"]: "Number of tries: 0",
+    ["MESSAGE_STAGE5"]: "Loading language processing libraries...",
+    ["MESSAGE_STAGE6"]: "Load complete.",
+    ["MESSAGE_STAGE7"]: "Hello, human.",
+    ["MESSAGE_STAGE8"]: "Now it is your opportunity to have a second chance.",
+    ["MESSAGE_STAGE9"]: "Pass the 10 questions to save your race.",
+  };
+  return stages[stage];
+};
 
 const typeMessage = () => {
-    const MSG_HTML = document.querySelector('#start__message');
-    const message = `· · · Hi, human. Now, it's your opportunity to save your world. Are you ready? (Press any key to start)`;
+  // Typing speed
+  let speed = 100;
 
-    
+  // We'll generate two messages depending on the stage
+  const MSG_HTML_PRIMARY = document.querySelector("#start__message--primary");
+  const MSG_HTML_SECONDARY = document.querySelector(
+    "#start__message--secondary"
+  );
+
+  // Logic to type the messages
+  if (x.value < STAGES.length) {
+    let stage = STAGES.at(x.value);
+    let message = getStageMessage(stage) + "           ";
     if (i.value < message.length) {
-        MSG_HTML.innerHTML += message.at(i.value)
-        i.value += 1;
-        console.log(message.at(i.value))
-        setTimeout(typeMessage, 100)
+      if (x.value < 9) {
+        MSG_HTML_SECONDARY.innerHTML += message.at(i.value);
+      } else {
+        // "Hello, human." message after loading messages
+        MSG_HTML_PRIMARY.innerHTML += message.at(i.value);
+      }
+      i.value += 1;
+      // Here, I wanted to speed up on MESSAGE_STAGE1
+      setTimeout(typeMessage, x.value === 3 ? speed / 2 : speed);
     }
+    // Finally, we clean the HTML and show the next message
+    if (i.value === message.length) {
+      MSG_HTML_PRIMARY.innerHTML = "";
+      MSG_HTML_SECONDARY.innerHTML = "";
+      i.value = 0;
+      x.value += 1;
+    }
+  }
+};
+
+const listenPressAnyKey = () => {
+    document.addEventListener('keydown', () => {
+        console.log('Pusing to the Test view...');
+        router.push('/test');
+    })
 }
 
-onMounted(() => typeMessage());
-
+onMounted(() => {
+    typeMessage()
+    listenPressAnyKey();
+});
 </script>
 
 <style lang="scss" scoped>
-
+@import "../scss/app.scss";
 .start__container {
-    width: 450px;
-    height: auto;
+  width: 450px;
+  height: auto;
 }
 </style>
